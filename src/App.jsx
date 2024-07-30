3f802423c487e50e1ffd31ef7048198f667f9859
dandanii22 <dandanii0816@gmail.com>
2024-07-30 15:31:05 +0900

    이미지 경로, HashRouter 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import BlogCreate from "./components/BlogCreate";
 import BlogDetails from "./components/BlogDetails";
 import NotFound from "./components/NotFound";
 import FixContent from "./components/FixContent";
-// import CommentCreate from "./components/CommentCreate";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<BlogCreate />} />
             <Route path="/blog/:id" element={<BlogDetails />} />
-            {/* <Route path="/CommentCreate/:id" element={<CommentCreate />} /> */}
             <Route path="/FixContent/:id" element={<FixContent />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
